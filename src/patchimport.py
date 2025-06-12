32c5c22a8de02d5f152482cf63bcaba6f52f9a3f
Luigi Finetti <luigi@finetti.com.ar>
2025-06-12 18:28:18 -0300

    fix: Appending at end of file now works

--- src/patchimport.py
+++ src/patchimport.py
@@ -13,7 +13,7 @@ def modify_and_import(module_name, package, modification_func):
     spec = util.find_spec(module_name, package)
     source = spec.loader.get_source(module_name)
     new_source = modification_func(source)
-    spec.origin = None # TODO: Make Jupyter stop finding the (now wrong) code on ??
+    spec.origin = None  # TODO: Make Jupyter stop finding the (now wrong) code on ??
     spec.cached = None
     module = util.module_from_spec(spec)
     codeobj = compile(new_source, f"<patched {module.__spec__.origin}>", "exec")
@@ -61,6 +61,7 @@ def parse_args(line: str) -> PatchArgs:
         module=args.module, start_line=args.start_line, end_line=args.end_line
     )
 
+
 def patcher(source, start_line, end_line, patch, log_function=print):
     lines = source.splitlines()
     patch_lines = patch.splitlines()
@@ -78,23 +79,29 @@ def patcher(source, start_line, end_line, patch, log_function=print):
         preview_lines += [
             f"{i+1:3}+{new_lines[i]}" for i in range(start_line, modified_end)
         ]
-        if (
-            end_line <= modified_end
-        ):  # We inserted at least what we deleted, so we print from new_lines
-            preview_lines += [
-                f"{i+1:3} {new_lines[i]}" for i in range(modified_end, modified_end + 3)
-            ]
-        else:  # We deleted more than we inserted, so we print from original lines
-            preview_lines += [
-                f"{i+1:3}-{lines[i]}" for i in range(modified_end, end_line)
-            ]
-            preview_lines += [
-                f"{i+1:3} {lines[i]}" for i in range(end_line, end_line + 3)
-            ]
+        try:
+            if end_line <= modified_end:
+                # We inserted at least what we deleted, so we print from new_lines
+                preview_lines += [
+                    f"{i+1:3} {new_lines[i]}"
+                    for i in range(modified_end, modified_end + 3)
+                ]
+            else:
+                # We deleted more than we inserted, so we print from original lines
+                preview_lines += [
+                    f"{i+1:3}-{lines[i]}" for i in range(modified_end, end_line)
+                ]
+                preview_lines += [
+                    f"{i+1:3} {lines[i]}" for i in range(end_line, end_line + 3)
+                ]
+        except IndexError:
+            # We fell off the end, and that's ok.
+            pass
         log_function(f"Patch applied from line {start_line+1} to {end_line+1}:\n")
         log_function("\n".join(preview_lines))
 
     return new_src
+
 
 def patchimport(line, cell):
     """
@@ -137,12 +144,19 @@ def patchimport(line, cell):
     modify_and_import(
         module,
         None,
-        partial(patcher, start_line=start_line - 1, end_line=end_line - 1, patch=cell, log_function=print),
+        partial(
+            patcher,
+            start_line=start_line - 1,
+            end_line=end_line - 1,
+            patch=cell,
+            log_function=print,
+        ),
     )
     print(
         f"REMEMBER TO DO `import {module}` OR `from {module} import ...` AFTER THIS MAGIC CALL!"
     )
     return
+
 
 # TODO: Add support for diffs
 
